c867c3f0467d209ea8731ae243f4b86538c9960a
Elena Cristescu <elenacristescu11@gmail.com>
2023-11-14 17:33:48 +0100

    add cookie message and implement button functionality to remove the cookie

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//document methods
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+//select Elements
+const header = document.querySelector('.header');
+//this creates a NodeList which doesn't update automatically
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+//get Elements by ...
+document.getElementById('section--1');
+//this creates a live HTMLColection which updates automatically
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+
+//create and insert Elements
+//.insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent('We use cookies for improved functionality and analytics.');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics.<button class = "btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+header.after(message);
+
+//delete Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message);
+  });
