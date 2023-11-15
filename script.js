359a03340b6d4bdf107a7a7515531cce23a742f0
Elena Cristescu <elenacristescu11@gmail.com>
2023-11-15 13:50:49 +0100

    example of the uses of DOM manipulation on Styles,Attributes and Classes

--- script.js
+++ script.js
@@ -57,16 +57,61 @@ message.classList.add('cookie-message');
 //message.textContent('We use cookies for improved functionality and analytics.');
 message.innerHTML =
   'We use cookies for improved functionality and analytics.<button class = "btn btn--close-cookie">Got it!</button>';
-// header.prepend(message);
-// header.append(message);
+//header.prepend(message);
+header.append(message);
 // header.append(message.cloneNode(true));
 // header.before(message);
-header.after(message);
+// header.after(message);
 
 //delete Elements
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
     message.remove();
+
     // message.parentElement.removeChild(message);
   });
+
+//STYLES
+message.style.backgroundColor = '#37383d';
+//message.style.width = '100%';
+
+console.log(message.style.color);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//ATTRIBUTES
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+//Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes
+logo.classList.add('c', 'd');
+logo.classList.remove('c', 'd');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//Don't use since it will overight
+//logo.className='c'
