404713453ff5440d00e241143a8073636915eb96
Elena Cristescu <elenacristescu11@gmail.com>
2023-11-19 19:08:39 +0100

    examples of DOM traversing

--- script.js
+++ script.js
@@ -205,3 +205,32 @@ logo.classList.contains('c');
 //   },
 //   false
 // );
+
+const h1 = document.querySelector('h1');
+
+//Going downwards : child elements
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); //direct children
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+//Going upwards : parent elements
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+//closest finds parents no mater how far they are in the dom tree
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: sibling elements
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+//for nodes
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el != h1) el.style.transform = 'scale(0.5)';
+});
