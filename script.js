81ea6d7b28260d1612bbb3bf2cc78ca7e2895c55
Elena Cristescu <elenacristescu11@gmail.com>
2024-01-21 15:18:26 +0100

    sticky navigation using scroll event

--- script.js
+++ script.js
@@ -89,6 +89,16 @@ const handleHover = function (e, opacity) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+//Sticky NAVIGATION
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function (e) {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
